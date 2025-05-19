dc3bd2f56b5e9ae3067eba29d626113e01290604
Yasin Cakal <whoisyasin@gmail.com>
2025-05-18 19:29:41 -0700

    recursive curry

--- js30/curry.js
+++ js30/curry.js
@@ -1,15 +1,59 @@
-function curry(fn) {
-  let accumulatedArgs = []
+// function curry(fn) {
+//   let accumulatedArgs = []
 
+//   return function curried(...args) {
+//     accumulatedArgs = [...accumulatedArgs, ...args]
+
+//     if (fn.length === accumulatedArgs.length) {
+//       const result = fn(...accumulatedArgs)
+//       accumulatedArgs = []
+//       return result
+//     } else {
+//       return curried
+//     }
+//   }
+// }
+
+function curry(func) {
   return function curried(...args) {
-    accumulatedArgs = [...accumulatedArgs, ...args]
+    const context = this // capture initial `this`
 
-    if (fn.length === accumulatedArgs.length) {
-      const result = fn(...accumulatedArgs)
-      accumulatedArgs = []
-      return result
+    if (args.length >= func.length) {
+      return func.apply(context, args)
     } else {
-      return curried
+      return function (...nextArgs) {
+        return curried.apply(context, [...args, ...nextArgs])
+      }
     }
+  }
+}
+
+// function recursiveCurry(fn) {
+//   if (args.length === fn.length) {
+//     return fn(...args)
+//   } else {
+//     return function (...newArgs) {
+//       recursiveCurry(...args, ...newArgs)
+//     }
+//   }
+// }
+
+export default function curry(func) {
+  function curried(context, args) {
+    return function (...nextArgs) {
+      const allArgs = [...args, ...nextArgs]
+
+      if (allArgs.length >= func.length) {
+        return func.apply(context, allArgs)
+      } else {
+        return curried(context, allArgs)
+      }
+    }
+  }
+
+  // Initial wrapper to capture `this` once at the start
+  return function (...initialArgs) {
+    const context = this
+    return curried(context, initialArgs)
   }
 }
