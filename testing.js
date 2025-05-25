156bdf45beb4a8a45de736d0940340dbbefe21b8
Yasin Cakal <whoisyasin@gmail.com>
2025-05-24 19:17:15 -0700

    day

--- testing.js
+++ testing.js
@@ -1,22 +1,16 @@
-const input = document.getElementById('input')
-const add = document.getElementById('add')
-const list = document.getElementById('todos')
+function timeLimit(fn: Fn, t: number): Fn {
+  return async function (...args) {
+    return new Promise(async (resolve, reject) => {
+      const rej = setTimeout(reject, t, 'Time Limit Exceeded')
 
-add.addEventListener('click', () => {
-  const text = input.value.trim()
-  if (!text) return
-  const li = document.createElement('li')
-  li.textContent = text
-  const del = document.createElement('button')
-  del.textContent = 'X'
-  del.addEventListener('click', () => li.remove())
-  list.appendChild(li)
-  li.appendChild(del)
-  input.value = ''
-})
-input.addEventListener('keydown', e => {
-  if (e.key === 'Enter') {
-    e.preventDefault()
-    add.click()
+      try {
+        const res = await fn(...args)
+        resolve(res)
+      } catch (err) {
+        reject(err)
+      } finally {
+        clearTimeout(rej)
+      }
+    })
   }
-})
+}
