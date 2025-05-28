ff862c12fff88e4b45cc78d98d0d8f3520c8c63c
Yasin Cakal <whoisyasin@gmail.com>
2025-05-28 15:28:51 -0700

    stacks

--- dsa.js
+++ dsa.js
@@ -356,4 +356,20 @@ class Queue {
   }
 }
 
+// Section 19: Stack Em Up With Stacks
+class Stack {
+  constructor() {
+    this.data = []
+  }
+  push(val) {
+    this.data.push(val)
+  }
+  pop() {
+    return this.data.pop()
+  }
+  peek() {
+    return this.data[this.data.length - 1]
+  }
+}
+
 //
