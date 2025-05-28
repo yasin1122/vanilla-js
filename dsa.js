f660dbf74f257e6211aa810e0d689e8dd60754ec
Yasin Cakal <whoisyasin@gmail.com>
2025-05-28 16:05:15 -0700

    queue from 2 stacks

--- dsa.js
+++ dsa.js
@@ -372,4 +372,35 @@ class Stack {
   }
 }
 
-//
+// Section 20: Two Become One
+class Queue {
+  constructor() {
+    this.stack1 = new Stack()
+    this.stack2 = new Stack()
+  }
+  add(val) {
+    this.stack1.push(val)
+  }
+  remove() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop())
+    }
+    const returnVal = this.stack2.pop()
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop())
+    }
+    return returnVal
+  }
+  peek() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop())
+    }
+    const returnVal = this.stack2.peek()
+
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop())
+    }
+    return returnVal
+  }
+}
