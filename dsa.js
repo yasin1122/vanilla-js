b7cb8007e46b8ee240a42b5548b855dc59999611
Yasin Cakal <whoisyasin@gmail.com>
2025-05-30 13:13:37 -0700

    Find the Midpoint

--- dsa.js
+++ dsa.js
@@ -512,3 +512,14 @@ class LinkedList {
     }
   }
 }
+
+// Section 22: Find the Midpoint
+function midpoint(list) {
+  let slow = list.getFirst()
+  let fast = list.getFirst()
+  while (fast.next && fast.next.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+  return slow
+}
