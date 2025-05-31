8258475ac8e592a13edf02a4f292d9de0beec45b
Yasin Cakal <whoisyasin@gmail.com>
2025-05-30 19:12:11 -0700

    bst insert

--- dsa.js
+++ dsa.js
@@ -601,6 +601,25 @@ function levelWidth(root) {
     cache.forEach(node => newCache.push(...node.children))
     cache = newCache
   }
-
   return counters
+}
+
+// Section 27: My Best Friend, Binary Search Trees
+class Node {
+  constructor(data) {
+    this.data = data
+    this.left = null
+    this.right = null
+  }
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data)
+    } else if (data < this.data) {
+      this.left = new Node(data)
+    } else if (data > this.data && this.right) {
+      this.right.insert(data)
+    } else if (data > this.data) {
+      this.right = new Node(data)
+    }
+  }
 }
