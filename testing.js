ddf827696494a3f80d2c72d9303875b7515b6ba4
Yasin Cakal <whoisyasin@gmail.com>
2025-06-03 12:57:50 -0700

    js

--- testing.js
+++ testing.js
@@ -1,5 +1,5 @@
 // convert back to js
-function timeLimit(fn: Fn, t: number): Fn {
+function timeLimit(fn, t) {
   return async function (...args) {
     return new Promise(async (resolve, reject) => {
       const rej = setTimeout(reject, t, 'Time Limit Exceeded')
