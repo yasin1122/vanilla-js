3d5c4b9e83d14289aeeb880828aa6e6a2dccf2dd
Yasin Cakal <whoisyasin@gmail.com>
2025-06-05 09:06:29 -0700

    day

--- testing.js
+++ testing.js
@@ -1,17 +1,2 @@
-// convert back to js
-function timeLimit(fn, t) {
-  return async function (...args) {
-    return new Promise(async (resolve, reject) => {
-      const rej = setTimeout(reject, t, 'Time Limit Exceeded')
-
-      try {
-        const res = await fn(...args)
-        resolve(res)
-      } catch (err) {
-        reject(err)
-      } finally {
-        clearTimeout(rej)
-      }
-    })
-  }
-}
+const list = [1, 2, 3, 4, 5, 6]
+console.log(list[list.length - 1])
